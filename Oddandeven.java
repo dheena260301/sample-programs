aa6b3284046d9012d2957b9d724f99d423cb7077
dheena260301 <109955203+dheena260301@users.noreply.github.com>
2022-08-23 13:34:53 +0530

    Add files via upload

---
+++ Oddandeven.java
@@ -0,0 +1,27 @@
+package test;
+
+public class Oddandeven {
+
+	public static void main(String[] args) {
+		int[]a= {11,12,14,13,15,17,16,19,18,10};
+		System.out.println("even");
+		for(int i=0;i<a.length;i++) {
+			
+			if(a[i]%2==0) {
+				System.out.println(a[i]);
+			}
+			}
+		System.out.println("odd");
+for(int i=0;i<a.length;i++) {
+			
+			if(a[i]%2!=0) {
+				System.out.println(a[i]);
+	}
+	
+		
+
+}
+	}
+}
+
+
